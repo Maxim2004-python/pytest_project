0b9ef8675a88aa2d6d8fb090501c35b9caa995b7
Maxim <maxkolobov2004.09v@gmail.com>
2024-01-25 03:10:02 +0500

    update test functions

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -5,6 +5,10 @@ from utils.arrs import get, my_slice
 
 def test_get():
     assert get([1, 2, 3], 1, "test") == 2
+    assert get([1, 2, 3, 4, 5], 3, "test") == 4
+    assert get([1, 2, 3, 4, 5, 6], -1, "test")
 
 def test_my_slice():
     assert my_slice([1, 2, 3, 4, 5], 2, -1) == [3, 4]
+    assert my_slice([1, 2, 3, 4, 5], 0) == [1, 2, 3, 4, 5]
+    assert my_slice([1, 2, 3, 4, 5], 1, 5) == [2, 3, 4, 5]
