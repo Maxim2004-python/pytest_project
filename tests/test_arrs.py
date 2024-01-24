425656c76ccd65d528a65fa290218d5ff0447366
Maxim <maxkolobov2004.09v@gmail.com>
2024-01-25 00:39:08 +0500

    create test my_slice

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,7 +1,10 @@
 import pytest
 
-from utils.arrs import get
+from utils.arrs import get, my_slice
 
 
 def test_get():
     assert get([1, 2, 3], 1, "test") == 2
+
+def test_my_slice():
+    assert my_slice([1, 2, 3, 4, 5], 2, -1) == [3, 4]
